3fb00e742bfba43dbb490a34bb3985001e5475b3
Sebastian Kemi <sebekish@gmail.com>
2015-05-14 18:58:08 +0200

    Cards

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,7 @@ var less = require('gulp-less')
 var path = require('path')
 var watch = require('gulp-watch')
 var autoprefixer = require('gulp-autoprefixer')
+var plumber = require('gulp-plumber');
 
 gulp.task('default', function() {
   // place code for your default task here
@@ -12,13 +13,14 @@ gulp.task('default', function() {
 
 gulp.task('less', function () {
     return gulp.src('less/main.less')
+        .pipe(plumber())
         .pipe(less({
             paths: [ path.join(__dirname, 'less', 'includes') ]
         }))
         .pipe(gulp.dest('public/css'))
 })
 
-gulp.task('prefixer', function() {
+gulp.task('prefixer', ['less'], function() {
     return gulp.src('public/css/main.css')
         .pipe(autoprefixer({
             browsers: ['last 2 versions'],
